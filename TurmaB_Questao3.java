ad758f3273bcaa7108733e90425bc45c0cec43dd
Sabrina Miranda <binacri.mi@gmail.com>
2022-03-06 18:22:19 -0300

    Subindo atualização dos últimos ajustes de documentação

--- TurmaB_Questao3.java
+++ TurmaB_Questao3.java
@@ -5,54 +5,49 @@ import java.util.ArrayList;
 public class TurmaB_Questao3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a string de abertura: ");
         String input = scanner.nextLine();
         scanner.close();
         
         
-     
-        // Retirando os espaços " " com replace().
+        // Retirando os espaços " " da string utilizando replace().
+         
         input = input.replace(" ", "");
 
-        
-        // Lista para guardar as substrings que foram divididas pela raiz quadrade da string.
+        /** 
+         * Lista para guardar a grid, ou seja, organizar os componentes divididos pela raiz quadrada.
+         * */ 
         List<String> subs = new ArrayList<>();
+        String result = "";
 
-        String finalStr = "";
 
-
-        int raizInput = (int) Math.ceil(Math.sqrt(input.length()));  // Cáculo da raiz quadrada arredondado para cima
+        // Cáculo da raiz quadrada.
+        int raizInput = (int) Math.ceil(Math.sqrt(input.length())); 
         
 
-         
-         // Dividindo as substrings(linhas do grid) com base na raiz quadrada do tamanho da string
+        // Criando e organizando em divisões a grid da string, a partir da raiz quadrada.
         for (int start = 0; start <= input.length(); start += raizInput) {
-
             if(start+raizInput > input.length()){
-
                 subs.add(input.substring(start, start+(input.length()) - start));
             }
-
             else 
                 subs.add(input.substring(start, start+raizInput));
         }
 
 
-        
-         
-         // Junção das colunas do grid para formar a string final de acordo com os critérios
+        // Utilizando as regras para junção das colunas do grid, em direção ao resultado final.
         for (int i = 0; i < subs.size(); i++) {
             for (int j = 0; j < raizInput; j++) {
                 
                 if(subs.get(j).length() > i){
-                    finalStr += subs.get(j).toCharArray()[i];
+                    result += subs.get(j).toCharArray()[i];
                 }
             }
-            finalStr += " ";
+            result += " ";
         }
 
-        
-         
-        System.out.println(finalStr);
+        // Resultado final
+        System.out.println("\nSaida: "+result);
     }
 
 }
