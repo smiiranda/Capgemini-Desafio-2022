2ef7c2e98db60b1dae41925490a6444a27fa8bbb
Sabrina Miranda <binacri.mi@gmail.com>
2022-03-06 17:25:36 -0300

    Organizando o código

--- TurmaB_Questao3.java
+++ TurmaB_Questao3.java
@@ -9,39 +9,37 @@ public class TurmaB_Questao3 {
         scanner.close();
         
         
-         // Retirando os espaços com replace().
-         
+     
+        // Retirando os espaços " " com replace().
         input = input.replace(" ", "");
 
         
-         // Lista para guardar as substrings que foram divididas pela raiz quadrade da string
-         
+        // Lista para guardar as substrings que foram divididas pela raiz quadrade da string.
         List<String> subs = new ArrayList<>();
 
         String finalStr = "";
 
 
-        
-         // Cáculo da raiz quadrada arredondado para cima
-         
-        int raizInput = (int) Math.ceil(Math.sqrt(input.length())); 
+        int raizInput = (int) Math.ceil(Math.sqrt(input.length()));  // Cáculo da raiz quadrada arredondado para cima
         
 
-        
-         // Dividindo as substrings(linhas do grid) com base na raiz quadrada do tamanho da string
          
+         // Dividindo as substrings(linhas do grid) com base na raiz quadrada do tamanho da string
         for (int start = 0; start <= input.length(); start += raizInput) {
+
             if(start+raizInput > input.length()){
+
                 subs.add(input.substring(start, start+(input.length()) - start));
             }
+
             else 
                 subs.add(input.substring(start, start+raizInput));
         }
 
 
         
-         // Junção das colunas do grid para formar a string final de acordo com os critérios
          
+         // Junção das colunas do grid para formar a string final de acordo com os critérios
         for (int i = 0; i < subs.size(); i++) {
             for (int j = 0; j < raizInput; j++) {
                 
@@ -53,7 +51,6 @@ public class TurmaB_Questao3 {
         }
 
         
-         // Impressão da String final encriptada.
          
         System.out.println(finalStr);
     }
